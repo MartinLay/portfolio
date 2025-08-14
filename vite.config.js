851a3a2eaf379a557c2121d46dc1001f4d2b8cea
Martin Lay <74428950+MartinLay@users.noreply.github.com>
2025-08-14 19:16:17 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// ganti 'my-portfolio' dengan nama repository kamu
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  base: '/portfolio/',
 })
